999f40752c707b6272478aee0f4871c694cb11f6
ShogoTakagi <t1610380@edu.cc.uec.ac.jp>
2021-06-01 12:09:13 +0900

    Create code snippet

--- Assets/VITC/Scripts/DecodeMock.cs
+++ Assets/VITC/Scripts/DecodeMock.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DecodeMock : MonoBehaviour
 {
+    [SerializeField] private Text vitcText;
+
+    [SerializeField] private RenderTexture inputTexture;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,65 @@ public class DecodeMock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (inputTexture != null)
+        {
+            // Get grayscale colors at first line
+            var colors = GetPixels(inputTexture);
+            Array.Resize<Color>(ref colors, inputTexture.width);
+            var grayscale = GetGrayScaleColor(colors);
+
+            // Read VITC
+            var result = ReadVitcLine(grayscale, 8);
+            vitcText.text = result.ToString(@"mm\:ss\.ff");
+        }
+    }
+
+    public TimeSpan ReadVitcLine(float[] vitcLine, int lineSize, float thresholdBlack = 0.1f,
+        float thresholdGray = 0.6f, float thresholdWhite = 0.9f)
+    {
+        // Decode here
+
+        // Temporary result by Time.time
+        //https://docs.unity3d.com/ja/2019.4/ScriptReference/Time-time.html
+        var temporary = Time.time;
+        return TimeSpan.FromSeconds(temporary);
+    }
+
+    private float[] GetGrayScaleColor(Color[] color)
+    {
+        var grayscale = new float[color.Length];
+        for (int i = 0; i < color.Length; i++)
+        {
+            grayscale[i] = color[i].grayscale;
+        }
+
+        return grayscale;
+    }
+
+    /// <summary>
+    /// Get color pixels from render texture
+    /// </summary>
+    /// <param name="rt"></param>
+    /// <returns></returns>
+    private Color[] GetPixels(RenderTexture rt)
+    {
+        // Cache active render texture
+        var currentRT = RenderTexture.active;
+
+        // Set active render texture to target
+        RenderTexture.active = rt;
         
+        // Set pixels to new texture by Texture2D.ReadPixels on active render texture
+        var texture = new Texture2D(rt.width, rt.height);
+        texture.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+        texture.Apply();
+
+        // Get colors from texture
+        var colors = texture.GetPixels();
+            
+        // Restore active render texture
+        RenderTexture.active = currentRT;
+
+        return colors;
     }
 }
